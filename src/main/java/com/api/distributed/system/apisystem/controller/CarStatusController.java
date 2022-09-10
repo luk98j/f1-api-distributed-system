eea08e01241e6ba921b44ab1a609729b878df8ff
Ukson <jankowskilukasz1337@gmail.com>
2022-09-10 14:55:45 +0200

    Code refactor

--- src/main/java/com/api/distributed/system/apisystem/controller/CarStatusController.java
+++ src/main/java/com/api/distributed/system/apisystem/controller/CarStatusController.java
@@ -1,20 +1,12 @@
 package com.api.distributed.system.apisystem.controller;
 
-import com.api.distributed.system.apisystem.dto.CarDamageList;
 import com.api.distributed.system.apisystem.dto.CarStatusList;
-import com.api.distributed.system.apisystem.entity.CarStatusEntity;
-import com.api.distributed.system.apisystem.repository.CarStatusRepository;
 import com.api.distributed.system.apisystem.service.CarStatusService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigInteger;
-import java.sql.Timestamp;
-import java.util.Date;
 
 @RestController
 @CrossOrigin
@@ -29,9 +21,4 @@ public class CarStatusController {
                                         @RequestBody CarStatusList carStatusList){
         return carStatusService.postData(key, carStatusList);
     }
-
-//    @GetMapping("/get-status")
-//    public ResponseEntity<CarStatusDto> getStatus(@RequestParam BigInteger sessionUid, @RequestParam String key){
-//        return carStatusService.getData(sessionUid, key);
-//    }
 }
--- src/main/java/com/api/distributed/system/apisystem/repository/CarStatusRepository.java
+++ src/main/java/com/api/distributed/system/apisystem/repository/CarStatusRepository.java
@@ -1,12 +1,13 @@
 package com.api.distributed.system.apisystem.repository;
 
-import com.api.distributed.system.apisystem.entity.CarDamageEntity;
 import com.api.distributed.system.apisystem.entity.CarStatusEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
 import java.util.List;
 
+@Repository
 public interface CarStatusRepository extends MongoRepository<CarStatusEntity, String> {
     CarStatusEntity findFirstBySessionUidAndKeyOrderByDateDesc(BigInteger sessionUid, String key);
     List<CarStatusEntity> findAllByKey(String key);
